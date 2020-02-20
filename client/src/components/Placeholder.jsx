1024063e669756ff40d55a263de0d5cbedb2b109
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-02-20 14:50:41 -0800

    added cors implemented apollo react apollo and was able to fetch a query from the express server via a react graphql request

--- client/src/components/Placeholder.jsx
+++ client/src/components/Placeholder.jsx
@@ -1,38 +1,83 @@
 import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import ggl from "graphql-tag";
+import { Query } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: `http://localhost:4000/graphql`
+});
+
+const PLACEHOLDER_QUERY = ggl`
+query PlaceholderQuery { 
+placeholder {
+        userId
+        id
+        title
+        body
+      }
+}
+`;
 
 class Placeholder extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [
-        {
-          userId: 1,
-          id: 123,
-          title: "post 1",
-          body: "content 1"
-        },
-        {
-          userId: 2,
-          id: 456,
-          title: "post 2",
-          body: "content 2"
-        }
+        // {
+        //   userId: 1,
+        //   id: 123,
+        //   title: "post 1",
+        //   body: "content 1"
+        // },
+        // {
+        //   userId: 2,
+        //   id: 456,
+        //   title: "post 2",
+        //   body: "content 2"
+        // }
       ]
     };
   }
   render() {
     return (
-      <div>
-        <h4 className="text-center mt-2">PLACEHOLDER</h4>
-        {this.state.posts.map(post => (
-          <div className="d-flex justify-content-center">
-            "POST userId": {post.userId}, "POST id": {post.id}, "POST title":{" "}
-            {post.title}, "POST body": {post.body}
-          </div>
-        ))}
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          <h4 className="text-center mt-2">Place Holder</h4>
+          <h5 className="text-center mt-1">Data: </h5>
+          <br />
+          <Query query={PLACEHOLDER_QUERY}>
+            {({ loading, error, data }) => {
+              if (loading) return <h4>Loading...</h4>;
+              if (error) return console.log(error);
+              console.log(data);
+              //       return <h1>TEST</h1>;
+
+              return data.placeholder.map(post => (
+                <div className="d-flex justify-content-center">
+                  "POST userId": {post.userId}, "POST id": {post.id}, "POST
+                  title": {post.title}, "POST body": {post.body}
+                </div>
+              ));
+            }}
+          </Query>
+          {/* {this.state.posts.map(post => (
+            <div className="d-flex justify-content-center">
+              "POST userId": {post.userId}, "POST id": {post.id}, "POST title":{" "}
+              {post.title}, "POST body": {post.body}
+            </div>
+          ))} */}
+        </div>
+      </ApolloProvider>
     );
   }
 }
 
 export default Placeholder;
+// {({ loading, error, data }) => {
+//         if (loading) return <h4>Loading...</h4>;
+//         if (error) return console.log(error);
+//         console.log(data);
+
+//         return <h1>TEST</h1>;
+//       }}
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = 4000 || process.env.PORT;
+const cors = require("cors");
 const graphqlHTTP = require("express-graphql");
 // schema - graphql api section
 const schema = require("./Schema/schema.js");
+
+// prevents CORS Error
+app.use(cors());
 
 // invoke schema with use method
 app.use(
@@ -20,3 +24,8 @@ app.use(
 app.listen(PORT, () => {
   console.log(`Express Server running on PORT: ${PORT}`);
 });
+
+/*
+"proxy": "http://localhost:3001",
+put before repo in package json
+*/
