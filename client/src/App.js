5d80f5caa50427e393593bfb4a9bee2042664d5b
Tarek El-Hajjaoui <tarekel96@gmail.com>
2020-02-19 21:02:09 -0800

    started modularizing the client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Articles from "./components/Articles.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h5 className="text-center mt-4">Hello World</h5>
+      <Articles />
     </div>
   );
 }
---
+++ client/src/components/Articles.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="text-center mt-3 ml-5">Search</button>
+      </div>
+    );
+  }
+}
+
+export default Articles;
